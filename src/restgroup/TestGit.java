de67f9ef8236195d0f4bbf6c887f571e3bef704a
Londonboy017 <jpjaydeep21@gmail.com>
2022-06-20 11:42:12 +0100

    print for a minute

--- src/restgroup/TestGit.java
+++ src/restgroup/TestGit.java
@@ -1,7 +1,11 @@
 package restgroup;
 
+import org.junit.Test;
+
 public class TestGit {
-    public static void main(String[] args) {
+    private int j=100;
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Test start by Jaydip");
 
         System.out.println("Test continue by Nirali");
@@ -10,6 +14,20 @@ public class TestGit {
         System.out.println("Test start by Riya");
         System.out.println("By Beena");
 
+        new TestGit().checkTime();
 
+        }
+@Test
+        public void checkTime() throws InterruptedException {
+
+    final long NANOSEC_PER_SEC = 1000*1000*1000;
+
+    long startTime = System.nanoTime();
+    while ((System.nanoTime()-startTime)< 1*15*NANOSEC_PER_SEC){
+        System.out.println("jaydip time");
     }
-}
+
+
+
+}}
+
